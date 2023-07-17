d250ad1907811fafe0762aa9fa70d76fd5a2ca3d
jungmin801 <130200440+jungmin801@users.noreply.github.com>
2023-07-17 23:43:48 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,13 +1,11 @@
 function solution(n) {
     // return Number.isInteger(Math.sqrt(n)) ? 1 : 2;
     
-    let answer = 0;
+    let answer = 2;
     for(let i = 1; i < n/2; i++){
         if (i ** 2 === n) {
-        answer = 1
-        break;
+        return answer = 1
         }
-        else answer = 2;
      }
     return answer;
 }
