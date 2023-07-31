ebe033ea7ff9ca071b7ef60ccb11702aba6e6a23
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-07-31 16:38:19 +0900

    [level 0] Title: 369게임, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,8 +1,8 @@
 function solution(order) {
-    let arr = [...String(order)].map(a => parseInt(a));
-    let answer = arr.filter( b => {
-        if( b === 3 || b === 6 || b === 9) return b
-    }).length
-    
-    return answer;
+    var total = 0
+    var answer = String(order).split("")
+    for(var idx in answer){
+        if(eval(answer[idx]) % 3 === 0 && eval(answer[idx])) total++
+    }
+    return total;
 }
