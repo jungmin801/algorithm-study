cc8c26c547fe3d16ff48b967875d43ce963fd0d8
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-08-06 22:07:29 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 4.78 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,11 +1,15 @@
 function solution(my_string) {
-    let str = [...my_string].map(a => parseInt(a)).join('');
-    let arr = str.split('NaN').filter(a => a !== "")
+    // 1. my_string을 split한 후 각 요소를 parseInt하면 '1' -> 1, 'a' -> NaN
+    // 2. 해당 배열의 요소를 join하여 하나의 문자로 만든다.
+    // 3. NaN을 기준으로 split 한다. 
+    
+    let str = my_string.split('').map(a => parseInt(a)).join(''); // NaNNaNNaN1NaN2NaNNaN34NaNNaNNaN
+    let arr = str.split('NaN').filter(a => a !== "").map( a => parseInt(a))
+    
+    console.log(str.split('NaN'))
+    
     let answer = 0;
-    for(let i=0; i<arr.length; i++){
-        answer += parseInt(arr[i])
-    }
-
+    arr.forEach( el => answer += el)
     return answer;
 
 }
