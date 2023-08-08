ed54695a5342d386007860b8e21685fd1d884d59
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-08-08 15:19:51 +0900

    [level 0] Title: 공 던지기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -0,0 +1,17 @@
+function solution(numbers, k) {
+    let arr = [];
+    if(numbers.length%2 === 0){
+        arr = numbers.filter((_,i) => i % 2 === 0);
+       
+    } else {
+       let first = numbers.filter((_,i) => i % 2 === 0);
+       let second = numbers.filter((_,i) => i % 2 !== 0);
+       arr = [...first, ...second] 
+    }
+
+    if(arr.length >= k) return arr[k-1];
+    else {
+        if(k % arr.length === 0) return arr[arr.length-1]
+        else return arr[(k % arr.length)-1] 
+    }
+}
