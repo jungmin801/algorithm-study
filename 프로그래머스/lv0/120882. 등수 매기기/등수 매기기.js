fbedb11218df48904e115a1f46c5b94f81d772e0
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-08-16 21:33:38 +0900

    [level 0] Title: 등수 매기기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,25 +1,52 @@
 function solution(score) {
-    const average = score.map( item => (item[0]+item[1]) / 2 )  // 75, 75, 40, 95, 95, 100, 20
-    const sortArr = [...average].sort((a,b) => b-a); //  100, 95, 95, 75, 75, 40, 20
-    
-    const rank = []; 
-    for(let i=0; i<sortArr.length; i++){
-        rank.push([sortArr[i],i+1]);
-        if(sortArr[i] === sortArr[i+1]) {
-         rank.push([sortArr[i+1],i+1]);
-         i += 1;   
+    let answer = [];
+    const average = score.map( item => (item[0]+item[1]) / 2 )
+    for(let i=0; i<average.length; i++){
+        let rank = 1;
+        for(let j=0; j<average.length; j++){
+            if(average[i]<average[j]){
+                rank++;
+            }
         }
-    }
-    
-    const answer = [];
-    for(let j=0; j<average.length; j++){
-        for(let z=0; z<rank.length; z++){
-            if(average[j] === rank[z][0]) {
-                answer.push(rank[z][1]);
-                break;
-            } else continue;
-        }
+        answer.push(rank);
     }
     
     return answer;
+    
+//     const average = score.map( item => (item[0]+item[1]) / 2 )  // 75, 75, 40, 95, 95, 100, 20
+//     const sortArr = [...average].sort((a,b) => b-a);
+
+//     const rank = []; 
+//     for(let i=0; i<sortArr.length; i++){
+//         rank.push([sortArr[i],i+1]);
+//         if(sortArr[i] === sortArr[i+1]) {
+//          rank.push([sortArr[i+1],i+1]);
+//          i += 1;   
+//         }
+//     }
+    
+//     let answer = [];
+//     average.forEach(value => {
+//         rank.forEach(item => {
+//             if (item[0] === value) {
+//                 answer.push(item[1]);
+//                 break;
+//             }
+//         });
+//     });
+    
+//     console.log(answer)
+    
+    
+// //     const answer = [];
+// //     for(let j=0; j<average.length; j++){
+// //         for(let z=0; z<rank.length; z++){
+// //             if(average[j] === rank[z][0]) {
+// //                 answer.push(rank[z][1]);
+// //                 break;
+// //             } else continue;
+// //         }
+// //     }
+    
+// //     return answer;
 }
