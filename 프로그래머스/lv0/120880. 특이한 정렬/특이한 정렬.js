1095a736ec7afcd5b972ad2fe7450e8aef744c6b
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-08-20 18:16:19 +0900

    [level 0] Title: 특이한 정렬, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,18 +1,17 @@
-function solution(numlist, n) {
-    let set = new Set();
-    numlist.sort((a,b) => b-a).map( x => set.add(Math.abs(n - x)));
-    let setArr = [...set].sort((a,b) => a-b);
-    
+function solution(numlist, n) {    
     let obj = {};
-    for(let value of set){
-       let temp = numlist.filter( el => (Math.abs(n - el) === value));
-       obj[value] = temp;                          
-    }
+    numlist.sort((a,b) => b-a)
+           .forEach( a => {
+               let diff = Math.abs(n - a);
+               if(!obj.hasOwnProperty(diff)){
+                   obj[diff] = [];
+               }
+               obj[diff].push(a);
+           });
     
     let answer = [];
     for(let index in obj){
         answer.push(...obj[index]);
     }
-    
     return answer;
 }
