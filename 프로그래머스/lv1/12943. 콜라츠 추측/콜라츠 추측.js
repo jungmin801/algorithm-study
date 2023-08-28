a107511163767cf07da4b712f39a3ceb0739b78f
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-08-28 17:53:12 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,11 +1,11 @@
 function solution(num) {
     let cnt = 0;
-    while(num !== 1) {
+    while(num !== 1 && cnt < 500) {
        if(num % 2 === 0 ) num = num / 2;
        else num = num * 3 + 1;
        cnt++;
    }
     
-    if(cnt > 500) return -1;
+    if(cnt === 500) return -1;
     else return cnt;
 }
