9dba84964f92f27e038d5f947255e01e9884be49
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-09-01 23:26:10 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.35 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,11 @@
+function solution(left, right) {
+    let answer = 0;
+    for(let i=left; i<=right; i++){
+        let arr = [1];
+        for(let j=2; j<=right; j++){
+            i % j === 0 ? arr.push(j) : '';
+        }
+        arr.length % 2 === 0 ? answer += i : answer -= i;
+    }
+    return answer;   
+}
