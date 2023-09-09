2dbd4f40629a56a3c870ad85382e1e95af677e54
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-09-09 10:55:57 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,11 +1,24 @@
 function solution(s) {
     let arr = s.split(' ');
-    let answer = arr.map( word => {
+    return answer = arr.map( word => {
        return word.split('').map((char, index) => {
-            if(index % 2 === 0) return char.toUpperCase();
-            else return char.toLowerCase();
+            return index % 2 === 0 ? char.toUpperCase() : char.toLowerCase();
         }).join('');
     }).join(' ');
-
-    return answer;    
 }
+
+// for문으로 풀기
+// function solution(s) {
+//     let arr = s.split(' ');
+//     let answer = [];
+//     for(let i=0; i<arr.length; i++){
+//         let word = '';
+//         for(let j=0; j<arr[i].length; j++){
+//             if(j % 2 === 0) {
+//                word += arr[i][j].toUpperCase();   
+//             } else word += arr[i][j].toLowerCase();
+//         }
+//         answer.push(word);
+//     }
+//     return answer.join(' ');
+// } 
