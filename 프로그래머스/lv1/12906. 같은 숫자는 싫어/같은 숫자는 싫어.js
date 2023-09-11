31d5ba45a6421825167576830fae99002c82801c
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-09-11 17:08:08 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 14.61 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,13 +1,9 @@
-function solution(arr) {
-
-    let answer = [];
-    answer.push(arr[0]);
-
-    for ( let i = 0; i < arr.length; i++ ) {
-        if ( answer[answer.length-1] !== arr[i] ) {
-            answer.push(arr[i]);
-        }
+function solution(arr){
+    let answer = []
+    let i = 0
+    while(i<arr.length){
+        if (arr[i] !== arr[i+1]) answer.push(arr[i])
+        i++
     }
-
-    return answer;
+    return answer
 }
