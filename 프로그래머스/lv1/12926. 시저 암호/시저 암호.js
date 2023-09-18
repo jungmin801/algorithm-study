1314a6c0798e9154cb51f0b84d0bf59977c9d0a2
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-09-18 20:25:09 +0900

    [level 1] Title: 시저 암호, Time: 2.48 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,19 +1,12 @@
 function solution(s, n) {
-    const nums = s.split('').map(el => el.charCodeAt());
-    const result = nums.map(el => {
-        if(el == 32) return el;
-        let copy = el;
-        el += n;
-        if(copy>=65 && copy<=90){
-            if (el>=65 && el<=90) return el;
-            else if(el>=91)return el-26;
-        } else if(copy>=97 && copy<=122){
-            if (el>=97 && el<=122) return el;
-            else if(el>=123) return el-26;
-        } 
-        
-    })
+    const result = s.split('')
+                   .map(el => {
+                        if(el == ' ') return el;
+                        const isUpperCase = el === el.toUpperCase();
+                        const base = isUpperCase? 65 : 97;
+                        const calc = (el.charCodeAt() - base + n) % 26;
+                        return String.fromCharCode(base + calc);
+                    }).join('');
     
-    const answer = result.map(num => String.fromCharCode(num)).join('');
-    return answer;
+    return result;
 }
