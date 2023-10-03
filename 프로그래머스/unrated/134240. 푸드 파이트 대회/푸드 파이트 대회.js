cb17f07b31d65f4a664808e7979a733ce5d2569c
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-10-03 23:24:34 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -3,9 +3,9 @@ function solution(food) {
     for(let i=1; i<food.length; i++){
         let chars = String(i).repeat(Math.trunc(food[i] / 2));
         result += chars;
-        if(i === food.length - 1) result += '0';
     }
     
-    return result + result.split('').slice(0,result.length-1).reverse().join('');
-    
+    return result + "0" + result.split('')
+                           .reverse()
+                           .join('');
 }
