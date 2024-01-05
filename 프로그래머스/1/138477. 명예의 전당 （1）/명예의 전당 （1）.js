e81d237ab551f57c6d5f5e2af46ce4e933f16d6c
문정민 <130200440+jungmin801@users.noreply.github.com>
2024-01-05 19:10:32 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,35 @@
+function solution(k, score) {
+    let result = [score[0]];
+    let hallOfFame = [score[0]];
+    for(let i = 1 ; i <k; i++){
+        if(i >= score.length) break;
+        hallOfFame.push(score[i]);
+        result.push(Math.min(result[i-1], score[i]));
+    }
+    
+    for(let j=k; j<score.length; j++){
+        let min = Math.min(...hallOfFame);
+        if(min < score[j]) hallOfFame[hallOfFame.indexOf(min)] = score[j];
+        result.push(Math.min(...hallOfFame));
+}
+    return result;
+    
+    // 인덱스넘버 1부터 k-1까지 result와 명예의 전당에 k 개의 점수 채우기
+//     for(let i=1; i<k; i++){
+//         if(i >= score.length) break;
+//         if(score[i] < result[i-1]) result.push(score[i]);
+//         else(result.push(result[i-1]));
+//         hallOfFame.push(score[i]);
+        
+//     }
+    
+    // 인덱스넘버 k부터 score배열 끝까지 돌면서 다음 점수와 최소값 비교
+//     for(let j=k; j<score.length; j++){
+//         let min = Math.min(...hallOfFame);
+//         if(min < score[j]) hallOfFame[hallOfFame.indexOf(min)] = score[j];
+//         result.push(Math.min(...hallOfFame));
+//     }
+    
+//     return result;
+
+}
