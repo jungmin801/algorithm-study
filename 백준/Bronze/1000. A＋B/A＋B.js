119eb65d3c3307d14387177bbc43a7ad0cadf05b
문정민 <130200440+jungmin801@users.noreply.github.com>
2024-02-16 23:14:51 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,9 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const sum = input.reduce((acc, cur) => acc + cur);
+console.log(sum);
