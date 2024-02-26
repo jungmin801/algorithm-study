a2f5496fc016e6b85aa225d9d0e45cb06c67a41f
문정민 <130200440+jungmin801@users.noreply.github.com>
2024-02-26 13:56:18 +0900

    [Bronze V] Title: 나머지, Time: 120 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,13 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const [A, B, C] = input;
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
