306e83cf4d766fc5372a985c55d0d54abaa92e97
문정민 <130200440+jungmin801@users.noreply.github.com>
2024-02-26 13:38:14 +0900

    [Bronze V] Title: A×B, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,9 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const answer = input[0] * input[1];
+console.log(answer);
